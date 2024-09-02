bf41519c81e97122cfd9b8f0ac9bce6b9acc75df
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-09-02 16:30:16 +0600

    dashboard btn

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  goToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
 
   onLogout() {
     this.authService.logout();
